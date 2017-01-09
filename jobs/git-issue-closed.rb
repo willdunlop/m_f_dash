4ec7f32b0e3d54fd855fa1d3d8cedcf2f219a4be
willdunlop <william.dunlop.1993@gmail.com>
2017-01-09 11:59:33 +1100

    Added multiple data sets for burndown, burndown will also update more dynamically now

--- jobs/git-issue-closed.rb
+++ jobs/git-issue-closed.rb
@@ -1,11 +1,13 @@
 #!/usr/bin/env ruby
+#This job is designed to display the number of closed issues for the current sprint
+
 require 'rest-client'
 require 'json'
 require 'date'
 
-git_token = "blocked"
-git_owner = "blocked"
-git_project = "blocked"
+git_token = ENV["GIT_TOKEN"]
+git_owner = ENV["GIT_OWNER"]
+git_project = ENV["GIT_PROJECT"]
 #git_issue_label = "03.Proposal"
 
 ## Change this if you want to run more than one set of issue widgets
@@ -25,69 +27,41 @@ uriClosed2 = "https://api.github.com/repos/#{git_owner}/#{git_project}/issues?st
 
 
 SCHEDULER.every '1m', :first_in => 0 do |job|
-    puts "Getting #{uri} and #{uri2}"
     firstResponse = RestClient.get uri
     secondResponse = RestClient.get uri2
     issuesPage1 = JSON.parse(firstResponse.body, symbolize_names: true)
     issuesPage2 = JSON.parse(secondResponse.body, symbolize_names: true)
 
-    puts "Getting #{uriClosed1} and #{uriClosed2}"
     thirdResponse = RestClient.get uriClosed1
     fourthResponse = RestClient.get uriClosed2
     cIssuesPage1 = JSON.parse(thirdResponse.body, symbolize_names: true)
     cIssuesPage2 = JSON.parse(fourthResponse.body, symbolize_names: true)
     # puts "milestone: #{issuesPage1.url}"
     # puts cIssuesPage1
-    open_issues = issuesPage1.length + issuesPage2.length
     closed_issues = cIssuesPage1.length + cIssuesPage2.length
-    all_issues = open_issues + closed_issues
 
-    mileNo5Open = []
-    mileNo5Closed = []
+    currentMilestone = 7
+    currentMileClosed = []
 
-    issuesPage1.length.times do |i|
-      if issuesPage1[i][:milestone] != nil
-        puts issuesPage1[i][:milestone][:number]
-        if issuesPage1[i][:milestone][:number] == 5
-            mileNo5Open << 1
-        end
-      end
-    end
-
-    issuesPage2.length.times do |x|
-      if issuesPage2[x][:milestone] != nil
-        puts issuesPage2[x][:milestone][:number]
-        if issuesPage2[x][:milestone][:number] == 5
-            mileNo5Open << 1
-        end
-      end
-    end
 
     cIssuesPage1.length.times do |a|
       if cIssuesPage1[a][:milestone] != nil
-        if cIssuesPage1[a][:milestone][:number] == 5
-            mileNo5Closed << 1
+        if cIssuesPage1[a][:milestone][:number] == currentMilestone
+            currentMileClosed << 1
         end
       end
     end
     cIssuesPage2.length.times do |b|
       if cIssuesPage2[b][:milestone] != nil
-        if cIssuesPage2[b][:milestone][:number] == 5
-            mileNo5Closed << 1
+        if cIssuesPage2[b][:milestone][:number] == currentMilestone
+            currentMileClosed << 1
         end
       end
     end
-    # sprint5iss = []
-    # for x in issuesPage1
-    #   if x.milestone.number = 5
-    #     x >> sprint5iss
-    #   else
-    #   end
-    # end
 
 
     send_event(event_name, {
-            text: mileNo5Closed.count
+            text: currentMileClosed.count
         })
 
 end # SCHEDULER
--- jobs/git-issues.rb
+++ jobs/git-issues.rb
@@ -3,13 +3,14 @@ require 'rest-client'
 require 'json'
 require 'date'
 
-git_token = "blocked"
-git_owner = "blocked"
-git_project = "blocked"
+
+git_token = ENV["GIT_TOKEN"]
+git_owner = ENV["GIT_OWNER"]
+git_project = ENV["GIT_PROJECT"]
 #git_issue_label = "03.Proposal"
 
 ## Change this if you want to run more than one set of issue widgets
-event_name = "git_issues_labeled_defects"
+event_name = "git_issues_burndown"
 
 ## the endpoint we'll be hitting
 uri = "https://api.github.com/repos/#{git_owner}/#{git_project}/issues?state=open&page=1&per_page=100&access_token=#{git_token}"
@@ -35,6 +36,7 @@ end
 
 
 SCHEDULER.every '1m', :first_in => 0 do |job|
+    puts "WELCOME TO THE MENTALLY FRIENDLY PROJECT DASHBOARD"
     puts "Getting #{uri} and #{uri2}"
     firstResponse = RestClient.get uri
     secondResponse = RestClient.get uri2
@@ -47,75 +49,90 @@ SCHEDULER.every '1m', :first_in => 0 do |job|
     cIssuesPage1 = JSON.parse(thirdResponse.body, symbolize_names: true)
     cIssuesPage2 = JSON.parse(fourthResponse.body, symbolize_names: true)
 
-    puts "\e[31m+++MILESTONE+++\e[0m: #{issuesPage1[0][:milestone][:number]}"
-
-
-    # sprint5iss = []
-    mileNo5Open = []
-    mileNo5Closed = []
+    currentMilestone = 7
+    currentMileOpen = []
+    currentMileClosed = []
+    puts "\e[31mCurrent Milstone: \e[32m#{currentMilestone}\e[0m"
 
     issuesPage1.length.times do |i|
       if issuesPage1[i][:milestone] != nil
-        puts issuesPage1[i][:milestone][:number]
-        if issuesPage1[i][:milestone][:number] == 5
-            mileNo5Open << 1
+        if issuesPage1[i][:milestone][:number] == currentMilestone
+            currentMileOpen << 1
         end
       end
     end
+    puts "\e[33mRetreiving Open Issues from page 1\e[0m"
+
 
     issuesPage2.length.times do |x|
       if issuesPage2[x][:milestone] != nil
-        puts issuesPage2[x][:milestone][:number]
-        if issuesPage2[x][:milestone][:number] == 5
-            mileNo5Open << 1
+        if issuesPage2[x][:milestone][:number] == currentMilestone
+            currentMileOpen << 1
         end
       end
     end
+    puts "\e[33mRetreiving Open Issues from page 2\e[0m"
+
 
     cIssuesPage1.length.times do |a|
       if cIssuesPage1[a][:milestone] != nil
-        if cIssuesPage1[a][:milestone][:number] == 5
-            mileNo5Closed << 1
+        if cIssuesPage1[a][:milestone][:number] == currentMilestone
+            currentMileClosed << 1
         end
       end
     end
+    puts "\e[33mRetreiving Closed Issues from page 1\e[0m"
+
     cIssuesPage2.length.times do |b|
       if cIssuesPage2[b][:milestone] != nil
-        if cIssuesPage2[b][:milestone][:number] == 5
-            mileNo5Closed << 1
+        if cIssuesPage2[b][:milestone][:number] == currentMilestone
+            currentMileClosed << 1
         end
       end
     end
+    puts "\e[33mRetreiving Closed Issues from page 2\e[0m"
 
-    open_issues = mileNo5Open.count
-    closed_issues = mileNo5Closed.count
+
+    open_issues = currentMileOpen.count
+    closed_issues = currentMileClosed.count
     all_issues = open_issues + closed_issues
 
-    puts "\e[31mmileNo5: #{mileNo5.count} \e[0m"
-
-    optimal_issues = all_issues / 14
-
-    def points_array(all, optimal)
-      @points = []
-      until all < 0
-        all = all - optimal
-        points << all
-      end
-      puts points
-    end
-
-
-    points_array(all_issues , optimal_issues)
 
     ## Drop the first point value and increment x by 1
-    graph_points.shift
-    last_x -= 1
+    graph_points = []
+    straight_data = [
+      {x: 0, y: all_issues},
+      {x: 14, y: 0}
+    ]
+    test_data = [
+      {x: 1, y: 5},
+      {x: 2, y: 5},
+      {x: 3, y: 1},
+      {x: 7, y: 4},
+      {x: 11, y: 3},
+      {x: 14, y: 2},
+    ]
+
 
     ## Push the most recent point value
-    graph_points << { x: 14, y: points }
 
-    send_event(event_name, {
-            text: open_issues, points: @points
-        })
+
+    # send_event(event_name, {
+    #         text: open_issues, points: [straight_data, test_data]
+    #     })
+
+    series = [
+      {
+        name: "Expected",
+        data: straight_data
+      },
+      {
+        name: "Actual",
+        data: test_data
+      }
+    ]
+
+    send_event("The Burn", series: series)
+    #system "curl -d '{ \"auth_token\": \"YOUR_AUTH_TOKEN\",\"points\": [ #{straight_data}, #{test_data}]}' http://localhost:3030/widgets/git_issues_burndown"
 
 end # SCHEDULER
