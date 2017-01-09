c7ec54a3bc194843f824a89c7afbc6f07974ec33
willdunlop <william.dunlop.1993@gmail.com>
2017-01-10 10:38:12 +1100

    Continued migration from dashing Mgraph data type to the LineChart data type for the burndown

--- jobs/git-issues.rb
+++ jobs/git-issues.rb
@@ -27,19 +27,19 @@ uriMilestone = "https://api.github.com/repos/#{git_owner}/#{git_project}/milesto
 refCount = 0
 
 SCHEDULER.every '10s', :first_in => 0 do |job|
-    puts "\e[34mGetting #{uri} and #{uri2}"
+    puts "\e[34mGetting #{uri} and #{uri2}\e[0m"
     firstResponse = RestClient.get uri
     secondResponse = RestClient.get uri2
     issuesPage1 = JSON.parse(firstResponse.body, symbolize_names: true)
     issuesPage2 = JSON.parse(secondResponse.body, symbolize_names: true)
 
-    puts "\e[34mGetting #{uriClosed1} and #{uriClosed2}"
+    puts "\e[34mGetting #{uriClosed1} and #{uriClosed2}\e[0m"
     thirdResponse = RestClient.get uriClosed1
     fourthResponse = RestClient.get uriClosed2
     cIssuesPage1 = JSON.parse(thirdResponse.body, symbolize_names: true)
     cIssuesPage2 = JSON.parse(fourthResponse.body, symbolize_names: true)
 
-    puts "\e[34mGetting #{uriMilestone}"
+    puts "\e[34mGetting #{uriMilestone}\e[0m"
     mileResponse = RestClient.get uriMilestone
     mileData = JSON.parse(mileResponse.body, symbolize_names: true)
 
@@ -51,11 +51,11 @@ SCHEDULER.every '10s', :first_in => 0 do |job|
     puts "\e[38;5;182mMENTALLY FRIENDLY PROJECT DASHBOARD\e[0m"
     puts "\e[94mRefresh for #{git_project} count: \e[32m#{refCount}\e[0m"
     puts "\e[94mCurrent Milstone: \e[32m#{currentMilestone}\e[0m"
-    puts "\e[34mCalculating sprint date range"
-    puts "\e[94mToday: #{now}"
+    puts "\e[34mCalculating sprint date range\e[0m"
+    puts "\e[94mToday: #{now}\e[0m"
     mileData.length.times do |num|
       if mileData[num][:number] == currentMilestone
-        puts "\e[94mMilestone Due: #{mileData[num][:due_on]}"
+        puts "\e[94mMilestone Due: #{mileData[num][:due_on]}\e[0m"
         @mileDueStr = mileData[num][:due_on]
       end
     end
@@ -79,12 +79,16 @@ SCHEDULER.every '10s', :first_in => 0 do |job|
     end
     puts "\e[34mRetreiving Open Issues from page 2\e[0m"
 
+    issClosedDate = []
 
     cIssuesPage1.length.times do |a|
       if cIssuesPage1[a][:milestone] != nil
         if cIssuesPage1[a][:milestone][:number] == currentMilestone
             currentMileClosed << 1
-            puts cIssuesPage1[a][:closed_at]
+            puts "Issue: #{cIssuesPage1[a][:number]} Closed at: #{cIssuesPage1[a][:closed_at]}"
+            closedDateStr = cIssuesPage1[a][:closed_at]
+            closedDate = Date.parse closedDateStr
+            issClosedDate << closedDate.to_s
         end
       end
     end
@@ -107,11 +111,11 @@ SCHEDULER.every '10s', :first_in => 0 do |job|
 
     ## Drop the first point value and increment x by 1
     graph_points = []
-    points1 = [
+    expected = [
       {x: 0, y: all_issues},
       {x: 14, y: 0}
     ]
-    points2 = [
+    actual = [
       {x: 1, y: 5},
       {x: 2, y: 5},
       {x: 3, y: 1},
@@ -121,30 +125,57 @@ SCHEDULER.every '10s', :first_in => 0 do |job|
     ]
 ### Chart.js chart ##
 
-  sunday = now - now.wday
+  #points need to be an array of y axis values
+
   mileDue = Date.parse @mileDueStr
 
-  puts mileDue
-  puts mileDue - 1
+  puts "\e[94mStart of sprint #{currentMilestone}: #{mileDue - 11}\e[0m"
+  puts "\e[94mEnd of Sprint #{currentMilestone}: #{mileDue}\e[0m"
+
+  optimal_issues = all_issues.to_f / 14
+  def points_array(all, optimal)
+      @expect = []
+      until all < 0
+        all = all - optimal
+        @expect << all
+      end
+      #puts expect
+    end
+    points_array(all_issues , optimal_issues)
 
 
+   dateRange = []
+   eaDay = 0
+   until eaDay == 12
+     mileDueStr = mileDue - eaDay
+     dateRange << mileDueStr.to_s
+     eaDay = eaDay + 1
+   end
+   puts "Closed date arr: #{issClosedDate}"
+   #for each date in issClosedDate find it's position in dateRange
+   issClosedDate.length.times do |eaClosed|
+     dateRange.length.times do |eaDate|
+      if issClosedDate[eaClosed] == dateRange[eaDate]
+        puts "Its positiong is: #{dateRange[eaDate].index}"
+      end
+    end
+  end
 
-  puts dateRange
-
-    labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July']
+   puts "DATES ARE: #{dateRange}"
+   puts "I must know: #{Array.new(dateRange.length) { rand(40..80) }}"
 
     data = [
         {
-          label: 'First dataset',
-          data: Array.new(labels.length) { rand(40..80) },
-          backgroundColor: [ 'rgba(255, 99, 132, 0.2)' ] * labels.length,
-          borderColor: [ 'rgba(255, 99, 132, 1)' ] * labels.length,
+          label: 'Expected',
+          data: @expect,
+          backgroundColor: [ 'rgba(255, 99, 132, 0.2)' ] * dateRange.length,
+          borderColor: [ 'rgba(255, 99, 132, 1)' ] * dateRange.length,
           borderWidth: 1,
         }, {
           label: 'Second dataset',
-          data: Array.new(labels.length) { rand(40..80) },
-          backgroundColor: [ 'rgba(255, 206, 86, 0.2)' ] * labels.length,
-          borderColor: [ 'rgba(255, 206, 86, 1)' ] * labels.length,
+
+          backgroundColor: [ 'rgba(255, 206, 86, 0.2)' ] * dateRange.length,
+          borderColor: [ 'rgba(255, 206, 86, 1)' ] * dateRange.length,
           borderWidth: 1,
         }
       ]
@@ -152,8 +183,9 @@ SCHEDULER.every '10s', :first_in => 0 do |job|
 
     ## Push the most recent point value
 
+    send_event('burn', { labels: dateRange, datasets: data })
 
-    #send_event("burn", points: [points1, points2])
+    #send_event("burn", points: [expected, actual])
 
     # series = [
     #   {
